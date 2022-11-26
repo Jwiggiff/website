995ac6c48cbb30ea292241f0e61e4bf2c11ac315
Jwiggiff <friedman.josh03@gmail.com>
2022-11-26 15:48:00 -0500

    Add wave

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -23,7 +23,7 @@ export default function Landing({ onScrollIn }) {
         <span>Josh Friedman</span>
       </h1>
       <p>
-        👋 Hey! I'm Josh, a full stack developer based in Toronto. I'm currently
+        <span>👋</span> Hey! I'm Josh, a full stack developer based in Toronto. I'm currently
         studying Computer Engineering at Queen's University in Kingston.
       </p>
     </section>
