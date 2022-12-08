9044ad8ca37454f1ca2ae9dcd0afb30d0ef44ac5
Jwiggiff <friedman.josh03@gmail.com>
2022-12-07 23:44:01 -0500

    extract skillslist to own component

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,57 +1,11 @@
-import { useRef, useState } from "react";
+import SkillsList from "./SkillsList";
 import Heading from "./_components/Heading";
-import skills from "./_data/skills.yml";
 
-export default function Skills({ onScrollIn }) {
-  const shouts = [
-    "Click Me!",
-    "Ouch!",
-    "Stop it!",
-    "That really hurt!",
-    "Okay it's not funny anymore",
-    "Seriously?",
-    "This ISN'T funny!",
-    "Ughhhhh",
-    "You've got to have something better to do",
-    "Really? Nothing?",
-    "Come on!?",
-    "Don't you have a life?",
-    "Go outside maybe",
-    "Get off your computer for a bit",
-    "This is getting ridiculous",
-    "I'm not even mad, I'm just disappointed",
-  ];
-  const [shoutI, setShoutI] = useState(0);
-  const containerRef = useRef(null);
-
-  const onClick = () => {
-    if (shoutI < shouts.length - 1) setShoutI(shoutI + 1);
-    containerRef.current.classList.add("active");
-    containerRef.current.addEventListener("animationend", () =>
-      containerRef.current.classList.remove("active")
-    );
-  };
-
+export default function Skills() {
   return (
     <section className="skills" id="skills">
       <Heading>Skills</Heading>
-      <div
-        onClick={onClick}
-        ref={containerRef}
-        className="container float-text"
-        shout={shouts[shoutI]}
-        style={{ "--deg": (shoutI / (shouts.length - 1)) * 360 + "deg" }}
-      >
-        {Object.keys(skills).map((cat, index) => (
-          <div className="row" key={index}>
-            <ul className="skills-list">
-              {skills[cat].map((skill, i) => (
-                <li key={i}>{skill.split("(")[0]}</li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </div>
+      <SkillsList />
     </section>
   );
 }
---
+++ src/SkillsList.jsx
@@ -0,0 +1,53 @@
+import { useRef, useState } from "react";
+import skills from "./_data/skills.yml";
+
+export default function SkillsList() {
+  const shouts = [
+    "Click Me!",
+    "Ouch!",
+    "Stop it!",
+    "That really hurt!",
+    "Okay it's not funny anymore",
+    "Seriously?",
+    "This ISN'T funny!",
+    "Ughhhhh",
+    "You've got to have something better to do",
+    "Really? Nothing?",
+    "Come on!?",
+    "Don't you have a life?",
+    "Go outside maybe",
+    "Get off your computer for a bit",
+    "This is getting ridiculous",
+    "I'm not even mad, I'm just disappointed",
+  ];
+  const [shoutI, setShoutI] = useState(0);
+  const containerRef = useRef(null);
+
+  const onClick = () => {
+    if (shoutI < shouts.length - 1) setShoutI(shoutI + 1);
+    containerRef.current.classList.add("active");
+    containerRef.current.addEventListener("animationend", () =>
+      containerRef.current.classList.remove("active")
+    );
+  };
+
+  return (
+    <div
+      onClick={onClick}
+      ref={containerRef}
+      className="container float-text"
+      shout={shouts[shoutI]}
+      style={{ "--deg": (shoutI / (shouts.length - 1)) * 360 + "deg" }}
+    >
+      {Object.keys(skills).map((cat, index) => (
+        <div className="row" key={index}>
+          <ul className="skills-list">
+            {skills[cat].map((skill, i) => (
+              <li key={i}>{skill.split("(")[0]}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+}
